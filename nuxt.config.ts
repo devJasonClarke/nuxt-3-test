28a4ca48a2a07a2d8f4846fa82596c0ec7e3fbb9
devJasonClarke <jettison200@gmail.com>
2021-10-13 23:29:27 -0500

    removed vite config and added sass loader

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,4 @@ import { defineNuxtConfig } from 'nuxt3'
 
 export default defineNuxtConfig({
     css: ["~/assets/scss/main.scss"],
-    vite: {
-        css: {
-          preprocessorOptions: {
-            scss: {
-              additionalData: '@import "@/assets/scss/main.scss";',
-            },
-          },
-        },
-      },
 })
